d5666ae7ecb81984e8876f369370bd8d168ec9cc
iamgopinathan <gopinathan.l@vajraglobal.com>
2023-12-01 11:51:17 +0530

    firstbranch

---
+++ lib/newfile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class newfile extends StatefulWidget {
+  const newfile({super.key});
+
+  @override
+  State<newfile> createState() => _newfileState();
+}
+
+class _newfileState extends State<newfile> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
